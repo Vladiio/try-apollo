085cfaf02a480504f4f4bf1153b4a740e3a59ffd
Vlad <vladpochta5@gmail.com>
2018-03-27 22:47:26 +0300

    polling, refetching, networkstatus

--- src/components/DogPhoto.js
+++ src/components/DogPhoto.js
@@ -3,17 +3,36 @@ import { Query } from "react-apollo";
 
 import { GET_DOG_PHOTO } from "../queries";
 
-const DogPhoto = ({ error, loading, data }) => {
+const DogPhoto = ({
+  error,
+  loading,
+  data,
+  refetch,
+  networkStatus,
+  startPolling,
+  stopPolling
+}) => {
+
+  if (networkStatus === 4) return <p>Refetching...</p>;
   if (error) return <p>{error}</p>;
-  if (loading) return <p>Loading</p>;
+  if (loading) return <p>Loading</p>
+  
 
   return (
     <div>
       <img src={data.dog.displayImage} alt="" />
+      <button onClick={() => refetch()}>refetch</button>
     </div>
   );
 };
 
 export default ({ breed }) => (
-  <Query children={DogPhoto} query={GET_DOG_PHOTO} variables={{ breed }} />
+  <Query 
+    children={DogPhoto}
+    query={GET_DOG_PHOTO}
+    variables={{ breed }}
+    // pollInterval={500}
+    notifyOnNetworkStatusChange
+    skip={!breed} 
+  />
 );
